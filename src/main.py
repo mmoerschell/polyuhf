61191ecfe5ca2b7053ed21309ab25418249a6a5f
Michael Mörschell <mmorsche@gmail.com>
2026-01-12 14:32:13 +0100

    Ruff formatting

--- src/main.py
+++ src/main.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 
 import argparse
-from parsing.frontend import parse_program
+
+from parsing.frontend import parse_file
 
 if __name__ == "__main__":
     cli = argparse.ArgumentParser(description="Polynomial Universal Hash Functions")
     cli.add_argument("input", type=str, help="input file")
     args = cli.parse_args()
 
-    with open(args.input, mode="r", encoding="utf-8") as f:
-        input = f.read()
-    
-    program = parse_program(input)
+    program = parse_file(args.input)
     print(program)
--- src/parsing/frontend.py
+++ src/parsing/frontend.py
@@ -1,8 +1,10 @@
-from antlr4 import InputStream, CommonTokenStream
+from antlr4 import CommonTokenStream, InputStream
+
 from .PolyUHFLexer import PolyUHFLexer
 from .PolyUHFParser import PolyUHFParser
 
-def parse_program(text: str):
+
+def parse_string(text: str):
     input_stream = InputStream(text)
     lexer = PolyUHFLexer(input_stream)
     token_stream = CommonTokenStream(lexer)
@@ -14,3 +16,8 @@ def parse_program(text: str):
     # ast = ASTBuilderVisitor().visit(tree)
 
     return tree
+
+
+def parse_file(path: str):
+    with open(path, mode="r", encoding="utf-8") as f:
+        return parse_string(f.read())
