19bdb8b21ffe89d802d3483a787253e3f2a286db
Michael Mörschell <mmorsche@gmail.com>
2026-01-12 16:58:19 +0100

    Update description

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import argparse
 from parsing.frontend import parse_file
 
 if __name__ == "__main__":
-    cli = argparse.ArgumentParser(description="Polynomial Universal Hash Functions")
+    cli = argparse.ArgumentParser(description="DSL Compiler")
     cli.add_argument("input", type=str, help="input file")
     args = cli.parse_args()
 
