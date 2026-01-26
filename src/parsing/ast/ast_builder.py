ae9982f71c6aa31de4c6690f5ed09bb7183fc606
Michael Mörschell <mmorsche@gmail.com>
2026-01-26 16:41:00 +0100

    Build nodes for sub/div/exp, cleanup

--- src/parsing/ast/ast_builder.py
+++ src/parsing/ast/ast_builder.py
@@ -1,4 +1,15 @@
-from parsing.ast.nodes import Add, ArrayAccess, Function, Int, Mul, Reduction, Var
+from parsing.ast.nodes import (
+    Add,
+    ArrayAccess,
+    Div,
+    Function,
+    Int,
+    Mul,
+    Power,
+    Reduction,
+    Sub,
+    Var,
+)
 from parsing.PolyUHFParser import PolyUHFParser
 from parsing.PolyUHFVisitor import PolyUHFVisitor
 
@@ -11,57 +22,90 @@ class ASTBuilder(PolyUHFVisitor):
     # Visit a parse tree produced by PolyUHFParser#function.
     def visitFunction(self, ctx: PolyUHFParser.FunctionContext):  # noqa: N802
         identifiers = [ter.getText() for ter in ctx.IDENTIFIER()]
-        expr = self.visit(ctx.expr())
-        return Function(identifiers[0], identifiers[1:], expr)
+        name = identifiers[0]
+        params = identifiers[1:]
+        body = self.visit(ctx.expr())
+        return Function(name, params, body)
 
     # Visit a parse tree produced by PolyUHFParser#expr.
     def visitExpr(self, ctx: PolyUHFParser.ExprContext):  # noqa: N802
-        # TODO!
+        # TODO remove?
         return self.visitChildren(ctx)
 
-    # Visit a parse tree produced by PolyUHFParser#Add.
-    def visitAdd(self, ctx: PolyUHFParser.AddContext):  # noqa: N802
+    # Visit a parse tree produced by PolyUHFParser#AddSub.
+    def visitAddSub(self, ctx: PolyUHFParser.AddSubContext):  # noqa: N802
         # left-associative fold
-        nodes = [self.visit(child) for child in ctx.mulExpr()]
+        nodes = [self.visit(child) for child in ctx.mulDivExpr()]
         if len(nodes) == 1:
             return nodes[0]
         node = nodes[0]
-        for n in nodes[1:]:
-            node = Add(node, n)
+        for i, n in enumerate(nodes[1:]):
+            op = ctx.getChild(2 * i + 1).getText()  # operator token: '+' or '-'
+            if op == "+":
+                node = Add(node, n)
+            elif op == "-":
+                node = Sub(node, n)
+            else:
+                raise RuntimeError(f"Invalid AddSub operator '{op}'")
         return node
 
-    # Visit a parse tree produced by PolyUHFParser#Mul.
-    def visitMul(self, ctx: PolyUHFParser.MulContext):  # noqa: N802
-        nodes = [self.visit(child) for child in ctx.primary()]
+    # Visit a parse tree produced by PolyUHFParser#MulDiv.
+    def visitMulDiv(self, ctx: PolyUHFParser.MulDivContext):  # noqa: N802
+        # left-associative fold
+        nodes = [self.visit(child) for child in ctx.exponentExpr()]
         if len(nodes) == 1:
             return nodes[0]
         node = nodes[0]
-        for n in nodes[1:]:
-            node = Mul(node, n)
+        for i, n in enumerate(nodes[1:]):
+            op = ctx.getChild(2 * i + 1).getText()  # '*' or '/'
+            if op == "*":
+                node = Mul(node, n)
+            elif op == "/":
+                node = Div(node, n)
+            else:
+                raise RuntimeError(f"Invalid MulDiv operator '{op}'")
         return node
+
+    # Visit a parse tree produced by PolyUHFParser#Exponent.
+    def visitExponent(self, ctx: PolyUHFParser.ExponentContext):  # noqa: N802
+        # Recalll rule: base (^ exponent)?
+        base = self.visit(ctx.primary())
+        if ctx.expr():
+            exponent = self.visit(ctx.expr())
+            return Power(base, exponent)
+        return base
+
+    # Visit a parse tree produced by PolyUHFParser#UnaryMinus.
+    def visitUnaryMinus(self, ctx: PolyUHFParser.UnaryMinusContext):  # noqa: N802
+        value = self.visit(ctx.primary())
+        # Note: unary minus is represented as multiplication
+        # by -1. This is only used for indices, not bigints.
+        # Any C compiler will happily optimize it away.
+        return Mul(Int(-1), value)
 
     # Visit a parse tree produced by PolyUHFParser#Parentheses.
     def visitParentheses(self, ctx: PolyUHFParser.ParenthesesContext):  # noqa: N802
+        # Remove parentheses
         return self.visit(ctx.expr())
 
-    # Visit a parse tree produced by PolyUHFParser#ReductionExpr.
-    def visitReductionExpr(self, ctx: PolyUHFParser.ReductionExprContext):  # noqa: N802
-        op = ctx.op.text  # type: ignore
-        var = ctx.IDENTIFIER().getText()
-        expressions = [self.visit(expr) for expr in ctx.expr()]
-        start, stop, step, body = expressions
-        return Reduction(op, var, start, stop, step, body)
-
-    # Visit a parse tree produced by PolyUHFParser#ArrayExpr.
-    def visitArrayExpr(self, ctx: PolyUHFParser.ArrayExprContext):  # noqa: N802
-        identifier = ctx.IDENTIFIER().getText()
-        expr = self.visit(ctx.expr())
-        return ArrayAccess(identifier, expr)
+    # Visit a parse tree produced by PolyUHFParser#IntExpr.
+    def visitIntExpr(self, ctx: PolyUHFParser.IntExprContext):  # noqa: N802
+        return Int(int(ctx.INT().getText()))
 
     # Visit a parse tree produced by PolyUHFParser#IdentifierExpression.
     def visitIdentifierExpression(self, ctx: PolyUHFParser.IdentifierExpressionContext):  # noqa: N802
         return Var(ctx.IDENTIFIER().getText())
 
-    # Visit a parse tree produced by PolyUHFParser#IntExpr.
-    def visitIntExpr(self, ctx: PolyUHFParser.IntExprContext):  # noqa: N802
-        return Int(int(ctx.INT().getText()))
+    # Visit a parse tree produced by PolyUHFParser#ArrayExpr.
+    def visitArrayExpr(self, ctx: PolyUHFParser.ArrayExprContext):  # noqa: N802
+        identifier = ctx.IDENTIFIER().getText()
+        index = self.visit(ctx.expr())
+        return ArrayAccess(identifier, index)
+
+    # Visit a parse tree produced by PolyUHFParser#ReductionExpr.
+    def visitReductionExpr(self, ctx: PolyUHFParser.ReductionExprContext):  # noqa: N802
+        # For bigints: '*' or '+'
+        op = ctx.op.text  # type: ignore
+        var = ctx.IDENTIFIER().getText()
+        start, stop, step, body = [self.visit(e) for e in ctx.expr()]
+        return Reduction(op, var, start, stop, step, body)
--- src/parsing/ast/nodes.py
+++ src/parsing/ast/nodes.py
@@ -3,34 +3,60 @@ from typing import List
 
 # ---------- Base nodes ----------
 
+
 class ASTNode:
     pass
 
+
 class Expr(ASTNode):
     pass
+
 
 @dataclass
 class Int(Expr):
     value: int
 
+
 @dataclass
 class Var(Expr):
     name: str
+
 
 @dataclass
 class Add(Expr):
     left: Expr
     right: Expr
 
+
+@dataclass
+class Sub(Expr):
+    left: Expr
+    right: Expr
+
+
 @dataclass
 class Mul(Expr):
     left: Expr
     right: Expr
 
+
+@dataclass
+class Div(Expr):
+    left: Expr
+    right: Expr
+
+
+@dataclass
+class Power(Expr):
+    base: Expr
+    power: Expr  # could restrict this to Int
+
+
 @dataclass
 class ArrayAccess(Expr):
     array: str
     index: Expr
+
 
 @dataclass
 class Reduction(Expr):
@@ -41,11 +67,14 @@ class Reduction(Expr):
     step: Expr
     body: Expr
 
+
 # ---------- Program structure ----------
+
 
 @dataclass
 class Program(ASTNode):
     functions: List["Function"]
+
 
 @dataclass
 class Function(ASTNode):
