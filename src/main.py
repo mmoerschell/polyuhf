8a0323590dd7421e809cee6953bb85907fa454f1
Michael Mörschell <mmorsche@gmail.com>
2026-02-03 17:31:07 +0100

    Refactor

--- src/main.py
+++ src/main.py
@@ -3,14 +3,12 @@
 import argparse
 from pprint import pprint
 
-from colorama import init
+import colorama
 
 from parsing.frontend import parse_file
 
-init(autoreset=True)  # resets terminal color after each print
-
-
 if __name__ == "__main__":
+    colorama.init(autoreset=True)  # resets terminal color after each print
     cli = argparse.ArgumentParser(description="DSL Compiler")
     cli.add_argument("input", type=str, help="input file")
     cli.add_argument("--verbose", "-v", action="store_true", help="Show IR")
