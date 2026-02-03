6b9bf5f5d3334ab826765ccebd21608d0c7f6ae8
Michael Mörschell <mmorsche@gmail.com>
2026-02-03 17:21:34 +0100

    Add verbose flag

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 import argparse
+from pprint import pprint
 
-# from pprint import pprint
 from colorama import init
 
 from parsing.frontend import parse_file
@@ -13,7 +13,9 @@ init(autoreset=True)  # resets terminal color after each print
 if __name__ == "__main__":
     cli = argparse.ArgumentParser(description="DSL Compiler")
     cli.add_argument("input", type=str, help="input file")
+    cli.add_argument("--verbose", "-v", action="store_true", help="Show IR")
     args = cli.parse_args()
 
     irprogam = parse_file(args.input)
-    # pprint(irprogam)
+    if args.verbose:
+        pprint(irprogam)
