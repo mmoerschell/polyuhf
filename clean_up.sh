184ab06a316302b535458e7dcd183e7a9e588465
Michael Mörschell <mmorsche@gmail.com>
2026-03-27 10:44:21 +0100

    Extend cleanup script

--- clean_up.sh
+++ clean_up.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+# Clear pycache
+
 # Source - https://stackoverflow.com/a/30659970
 # Posted by V. Gamula, modified by community. See post 'Timeline' for change history
 # Retrieved 2026-02-03, License - CC BY-SA 4.0
 
 find . | grep -E "(/__pycache__$|\.pyc$|\.pyo$)" | xargs rm -rf
+
+# rm generated files
+rm src/cpp/generated/*
