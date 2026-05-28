46022f5511ee7da48aabea70f26546b464e72863
Michael Mörschell <mmorsche@gmail.com>
2026-05-28 23:54:43 +0200

    More tests

--- src/cpp/load_store_tests.cpp
+++ src/cpp/load_store_tests.cpp
@@ -32,3 +32,27 @@ BOOST_AUTO_TEST_CASE(TestLoadStore) {
         BOOST_CHECK_EQUAL(check_bytes.back(), 0ull);
     }
 }
+
+BOOST_AUTO_TEST_CASE(TestSimpleSum) {
+    std::mt19937 rng(42);
+    std::uniform_int_distribution<uint8_t> dist(0, 255);
+    const size_t n_blocks = 500;
+    const size_t block_size = 14;
+    std::vector<uint8_t> data(n_blocks * block_size);
+    cpp_int acc(0);
+    for (auto &x : data)
+        x = dist(rng);
+
+    for (size_t i = 0; i < n_blocks; ++i) {
+        cpp_int data_i(0);
+        import_bits(data_i, data.cbegin() + 14 * i,
+                    data.cbegin() + 14 * (i + 1), 8, false);
+        acc = (acc + data_i) % c1163;
+    }
+    auto sum = simple_sum(data.data(), n_blocks);
+    std::array<uint8_t, 15> expected{}, actual{};
+    export_bits(acc, expected.begin(), 8, false);
+    export_15_bytes(actual.data(), &sum);
+    BOOST_CHECK_EQUAL_COLLECTIONS(actual.cbegin(), actual.cend(),
+                                  expected.cbegin(), expected.cend());
+}
